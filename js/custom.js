d4882eae37e41eb647faa1429d77bf5e1626f7ee
esauali <saulius.alisauskas@ericsson.com>
2013-11-11 23:15:05 +0100

    add slide, other theme

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+	console.log("done");
+	
+	$('.carousel').carousel({
+  		interval: 4000
+	});
+	// Navigation
+	$('ul.nav a').smoothScroll();
+
+	$('.navbar ul li').click(function(e) {
+		$('.navbar ul li').removeClass('active');       
+        $(this).addClass('active');
+       
+	});
+	console.log("done");
+
+});
