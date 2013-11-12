e0cb870e2ff13765614a0f98c01e5277f9403069
ilja.bobkevic <ilja.bobkevic@klarna.com>
2013-11-12 01:31:08 +0100

    Format, line feed

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,17 @@
-$( document ).ready(function() {
-	console.log("done");
+$(document).ready(function () {
+   console.log("done");
 
-	$('.carousel').carousel({
+   $('.carousel').carousel({
       interval: 3500
-	});
-	// Navigation
-	$('ul.nav a').smoothScroll();
+   });
+   // Navigation
+   $('ul.nav a').smoothScroll();
 
-	$('.navbar ul li').click(function(e) {
-		$('.navbar ul li').removeClass('active');
-        $(this).addClass('active');
+   $('.navbar ul li').click(function (e) {
+      $('.navbar ul li').removeClass('active');
+      $(this).addClass('active');
 
-	});
-	console.log("done");
+   });
+   console.log("done");
 
 });
