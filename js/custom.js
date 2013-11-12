a0106b4c4e2e207fcfca755b7b0c169ca96a5c0b
ilja.bobkevic <ilja.bobkevic@klarna.com>
2013-11-12 01:30:14 +0100

    Add curl command into pre. Increase carousel interval

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,16 @@
 $( document ).ready(function() {
 	console.log("done");
-	
+
 	$('.carousel').carousel({
-  		interval: 2000
+      interval: 3500
 	});
 	// Navigation
 	$('ul.nav a').smoothScroll();
 
 	$('.navbar ul li').click(function(e) {
-		$('.navbar ul li').removeClass('active');       
+		$('.navbar ul li').removeClass('active');
         $(this).addClass('active');
-       
+
 	});
 	console.log("done");
 
