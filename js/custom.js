4c946d3edddab7523bca39da727716951f8095b5
ilja.bobkevic <ilja.bobkevic@klarna.com>
2013-11-20 00:14:10 +0100

    Use "proper" js for feedback form

--- js/custom.js
+++ js/custom.js
@@ -14,14 +14,67 @@ $(document).ready(function () {
 
 });
 
-$('#form-contact').submit(function () { // catch the form's submit event
-   $.ajax({ // create an AJAX call...
-      data: $(this).serialize(), // get the form data
-      type: $(this).attr('method'), // GET or POST
-      url: $(this).attr('action'), // the file to call
-      success: function (response) { // on success..
-         $('#feedback-body').html(response); // update the DIV
+$("#form-contact").each(function () {
+   var form = $(this);
+
+   var submitBtn = $('button.submit', form);
+   var formMsg = $("textarea.message", form);
+   var formEmail = $("input.email", form);
+   var formName = $('input.name', form);
+   var formError = false;
+
+   submitBtn.click(function (e) {
+      formError = false;
+
+      checkText(formMsg);
+      checkEmail(formEmail);
+      checkText(formName);
+
+      if (formError)return false;
+      else {
+         $.post(form.attr('action'), {
+                  name: formName.val(),
+                  email: formEmail.val(),
+                  subject: "Kypado inquiery",
+                  msg: formMsg.val(),
+               },
+               function (data) {
+                  //alert("Data Loaded: " + data);
+                  $('#form-confirmation').html(data);
+                  if ($('#form-ok', '#form-confirmation').length > 0) {
+                     formMsg.val('');
+                     formEmail.val('');
+                     formName.val('');
+                     formSubject.val('');
+                  }
+
+                  $('#form-confirmation').hide().slideUp(0).slideDown(500).delay(5000).slideUp("slow", function () {
+                     $('#form-confirmation').html("data");
+                  });
+               });
       }
+      e.preventDefault();
    });
-   return false; // cancel original event to prevent form submitting
+
+   var checkText = function (node) {
+      if (node.length > 0) {
+         if (node.val().length < 2) {
+            formError = true;
+            highlightError(node);
+         }
+      }
+   }
+
+   var checkEmail = function (node) {
+      var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+      if (reg.test(node.val()) == false) {
+         formError = true;
+         highlightError(node);
+      }
+   }
+
+   var highlightError = function (node) {
+      console.log("shake" + node);
+      node.parent().addClass("has-error");
+   }
 });
