616ed8e089d55e9327c12024189fba24f46335ff
ilja.bobkevic <ilja.bobkevic@klarna.com>
2013-11-19 23:58:57 +0100

    Fix the feedback form

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {   
+$(document).ready(function () {
 
    $('.carousel').carousel({
       interval: 3500
@@ -10,6 +10,18 @@ $(document).ready(function () {
       $('.navbar ul li').removeClass('active');
       $(this).addClass('active');
 
-   });   
+   });
 
+});
+
+$('#form-contact').submit(function () { // catch the form's submit event
+   $.ajax({ // create an AJAX call...
+      data: $(this).serialize(), // get the form data
+      type: $(this).attr('method'), // GET or POST
+      url: $(this).attr('action'), // the file to call
+      success: function (response) { // on success..
+         $('#feedback-body').html(response); // update the DIV
+      }
+   });
+   return false; // cancel original event to prevent form submitting
 });
